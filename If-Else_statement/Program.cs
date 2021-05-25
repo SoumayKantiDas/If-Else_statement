5332162cdeea0d30bf14c15eae23df6325ff68b0
Soumay kanti Das <anikds2859@outlook.com>
2021-05-25 19:53:35 +0600

    Add project files.

---
+++ If-Else_statement/Program.cs
@@ -0,0 +1,27 @@
+﻿//if-else example
+
+using System;
+class ifText
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a character");
+        char c = (char)Console.Read()
+            if (char.IsLetter(c))
+        {
+            if (Char.IsLower(c))
+            {
+                Console.WriteLine("The charter is in lower case.");
+
+            }
+            else
+            {
+                Console.WriteLine("The charcter is uppercase.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("The charecter is not in alfabetic chrecter");
+        }
+    }
+}
